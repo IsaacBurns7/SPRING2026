5dd3e76b95003a630eec4634e76c17f07d5b2cb2
TheKillerKetchup <48397623+TheKillerKetchup@users.noreply.github.com>
2026-01-15 11:52:40 -0600

    fgetcsv implementation but with more use of regex

--- CSCE331/public/index.php
+++ CSCE331/public/index.php
@@ -137,7 +137,19 @@ Lastname:
 <?php 
     echo "<br/><br/><br/>TESTING PROC CSV"; 
     require_once("proc_csv.php");
-    proc_csv("../data/dat-doublequote-comma.csv",",","\"", "ALL"); //"1:3:5:7"
-    proc_csv("../data/dat-doublequote-comma.csv",",","\"", "1:3:5:7"); 
-    // proc_csv("test.csv",",","\"", "ALL");
+    proc_csv("../data/dat-doublequote-comma.csv",",","\"", "ALL");
+    proc_csv("../data/dat-doublequote-comma.csv",",","\"", "1:3:5:7");
+
+    proc_csv("../data/dat-doublequote-tab.csv","\t","\"", "ALL");
+    proc_csv("../data/dat-doublequote-tab.csv","\t","\"", "1:3:5:7");
+
+    proc_csv("../data/dat2-doublequote-comma.csv",",","\"", "ALL");
+    proc_csv("../data/dat2-doublequote-comma.csv",",","\"", "1:3:5:7");
+
+    proc_csv("../data/dat2-doublequote-tab.csv","\t","\"", "ALL");
+    proc_csv("../data/dat2-doublequote-tab.csv","\t","\"", "1:3:5:7");
+
+    proc_csv("../data/dat2-singlequote-tab.csv","\t","'", "ALL");
+    proc_csv("../data/dat2-singlequote-tab.csv","\t","'", "1:3:5:7");
+
 ?>
--- CSCE331/public/proc_csv.php
+++ CSCE331/public/proc_csv.php
@@ -5,19 +5,19 @@ function proc_csv($filename, $delimiter, $quote, $columns_to_show){
     $handle = fopen($filename, $mode) or die ("Cannot open $filename");
     $cols = [];
     $data = fgets($handle);
-    echo "<br/>RAW DATA: "; 
-    var_dump($data);
+    // echo "<br/>RAW DATA: "; 
+    // var_dump($data);
     $data = trim($data);
-    echo "<br/>non-raw data: "; 
-    var_dump($data);
+    // echo "<br/>non-raw data: "; 
+    // var_dump($data);
 
     /*
     3 cases: you're in quotes and you hit a delimiter, you're not in quotes and you hit a delimiter
     you're 
     */
-    $data = my_fgetcsv("$data", $delimiter, $quote);
-    echo "<br/>processed array: ";
-    var_dump($data);
+    $data = my_fgetcsv2("$data", $delimiter, $quote);
+    // echo "<br/>processed array: ";
+    // var_dump($data);
 
     // $data = preg_split("/$delimiter/", $data);
     // echo "<br/>DATA AFTER SPLIT BY DELIMITER: $delimiter ";
@@ -30,12 +30,14 @@ function proc_csv($filename, $delimiter, $quote, $columns_to_show){
             $cols[$k] = $k; 
         }
     }elseif(preg_match($pattern, $columns_to_show)) { 
-        $cols = preg_split(":", $columns_to_show);    
+        $cols = preg_split("/:/", $columns_to_show);
+        // echo "<br/>COLUMNS RAW: $columns_to_show";    
+        // echo "<br/>COLUMNS: $cols";
     }else{
         die ("$columns_to_show did not match pattern \'ALL\' or $reg");
     }
-    echo "<br/>";
-    var_dump($cols);
+    // echo "<br/>";
+    // var_dump($cols);
 
     echo "<table  border=\"1\">\n";
     echo "<tr>\n";
@@ -45,7 +47,7 @@ function proc_csv($filename, $delimiter, $quote, $columns_to_show){
     echo "</tr>\n";
     while ($data = fgets($handle)) {
         echo "<tr>\n";
-        $data_cols = my_fgetcsv($data,$delimiter,$quote);
+        $data_cols = my_fgetcsv2($data,$delimiter,$quote);
         foreach ($cols as $index) {
             echo "  <td> ".$data_cols[$index]." </td>\n";
         }
@@ -54,7 +56,7 @@ function proc_csv($filename, $delimiter, $quote, $columns_to_show){
 }
 
 function my_fgetcsv($line, $delimiter = ",", $enclosure = '"'){
-    echo "<br/>";
+    // echo "<br/>";
     $fields = [];
     $field = '';
     $in_quotes = false;
@@ -82,6 +84,44 @@ function my_fgetcsv($line, $delimiter = ",", $enclosure = '"'){
     $fields[] = $field;
 
     return $fields;
+}
+
+function my_fgetcsv2($line, $delimiter = ",", $enclosure = '"'){
+    $pattern = '/^\"[^\"]+"/';
+    $fields = [];
+    $matches = []; //should at most be 1 element given our specific regex
+    echo "<br/>";
+    while(strlen($line) != 0){
+        preg_match($pattern, $line, $matches);
+        if(count($matches) == 0){
+            //NOT a string -> delimiter is safe 
+            //look to delimiter or to newline
+            $pos = strpos($line, $delimiter);
+            if(!$pos){
+                $fields[] = $line; 
+                break; //end of line, can also do $line = '';  
+            }else{
+                $fields[] = substr($line, 0, $pos);
+                $line = substr($line, $pos+1);
+            }
+            // echo "not string!<br/>";
+        }else if(count($matches) == 1){
+            //string!! 
+            $len = strlen($matches[0]);
+            $fields[] = $matches[0];
+            $line = substr($line, $len+1); 
+            // echo "string!<br/>";
+        }else{
+            //something hath gone astray
+            die("Error: regex matched twice, not supposed to!");
+        }
+        // echo "Line: $line<br/>";
+        // foreach($fields as $field){
+        //     echo "$field    ";
+        // }
+        // echo "<br/>";
+    }
+    return $fields; 
 }
 
 ?>
